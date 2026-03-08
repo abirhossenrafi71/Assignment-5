8bdd9fde0d0bca698458ea8a349ddfdfeeda0f24
abirhossenrafi71 <abirhossenrafi71@gmail.com>
2026-03-09 01:12:20 +0600

    pirority color dynamic

--- script/issue.js
+++ script/issue.js
@@ -1,19 +1,14 @@
-// const removeActive = () => {
-//     const activeBtn = document.querySelectorAll('.filter-btn');
-//     activeBtn.forEach((btn) => btn.classList.remove('filter-btn'))
-// }
-// removeActive();
+
 let allIssues = [];
 const loadAIssue = async () => {
     const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const data = await res.json();
 
     allIssues = data.data;
+    document.getElementById('issue-count').innerText = allIssues.length + " " + "Issues";
+
     displayIssues(allIssues);
 
-}
-const showAll = () => {
-    document.getElementById('issue-count').innerText = allIssues.length + " " +"Issues";
 }
 const showOpen = () => {
     const openIssues = allIssues.filter(issue => issue.status === "open");
@@ -33,19 +28,20 @@ const displayIssues = (issues) => {
     issueContainer.innerHTML = "";
     issues.forEach(issue => {
         const borderColor = issue.status === 'open' ? "border-[#00A96E]" : "border-[#A855F7]";
+        const priorityColor = issue.priority === 'high' ? "bg-red-100 text-red-600" : issue.priority === 'medium' ? "bg-yellow-100 text-yellow-600" : "bg-[#EEEFF2] text-[#9CA3AF]";
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
-        <div class="border-t-4 bg-white ${borderColor} min-h-[400px] rounded-md shadow">
+        <div class="border-t-4 bg-white ${borderColor} min-h-[400px] rounded-2xl shadow">
             <div class="space-y-5 p-4">
                 <div class="flex justify-between">
                     <img src="assets/Open-Status.png" alt="" srcset="">
-                    <div class="badge badge-soft badge-error">${issue.priority}</div>
+                    <div class="badge badge-soft ${priorityColor} ">${issue.priority}</div>
                 </div>
                 <h2 class="font-bold text-2xl">${issue.title}</h2>
                 <p>${issue.description}</p>
                 <div class="flex gap-2">
-                    <div class="badge badge-soft badge-error">Bug</div>
-                    <div class="badge badge-soft badge-warning">help wanted</div>
+                    <div class="badge badge-soft badge-error border border-error"><i class="fa-solid fa-bug"></i> Bug</div>
+                    <div class="badge badge-soft badge-warning border border-warning">help wanted</div>
                 </div>
 
             </div>
